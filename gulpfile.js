c6789392da4e4ff9e24befec86a1a80f5d5b8eca
Tenzin Changchup <tchangchup@gmail.com>
2017-01-09 22:40:32 -0500

    gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+ watch = require('gulp-watch'),
+ postcss = require('gulp-postcss'),
+ autoprefixer = require('autoprefixer'),
+ cssvars = require('postcss-simple-vars'),
+ nested = require('postcss-nested');
 
 
 gulp.task('default', function() {
@@ -11,8 +15,20 @@ gulp.task('html', function() {
 });
 
 
+gulp.task('styles', function() {
+  // console.log('Imagine Sass or PostCss tasks file')
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
 gulp.task('watch', function() {
   watch('./app/index.html', function(){
     gulp.start('html');
-  })
-})
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+
+});
